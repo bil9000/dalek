b04958fb96e8d7d853ed69c6fc199a0dfdb1abe4
bill.koplitz <bill.koplitz@candasolutions.com>
2021-07-04 01:06:39 -0400

    add poidnfo gitrepository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dalek
- must kill the doctor
+## must kill the doctor
+
+spawned by experimentation with the eksctl gitops featured owned by 'dr. who.
+
+you know what happens next.
+
+
---
+++ service-account.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+CLUSTER_NAME=ss11
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+OIDC_PROVIDER=$(aws eks describe-cluster --name $CLUSTER_NAME --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+read -r -d '' TRUST_RELATIONSHIP <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${ACCOUNT_ID}:oidc-provider/${OIDC_PROVIDER}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${OIDC_PROVIDER}:sub": "system:serviceaccount:<my-namespace>:<my-service-account>"
+        }
+      }
+    }
+  ]
+}
+EOF
+echo "${TRUST_RELATIONSHIP}" > trust.json
